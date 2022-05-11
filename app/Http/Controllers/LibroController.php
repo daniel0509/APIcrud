343054c02a83d62ffed73e8f1b171d99b59b051a
DANIEL IVAN DE LEON ARZATE <83359146+daniel0509@users.noreply.github.com>
2022-05-11 12:47:59 -0500

    create_api

---
+++ app/Http/Controllers/LibroController.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Libro;
+use Carbon\Carbon;
+
+class LibroController extends Controller{
+
+    public function index(){
+        $datosLibro= Libro::all();
+        return response()->json($datosLibro);
+    }
+
+    public function guardar(Request $request){
+
+        $datosLibro= new Libro;
+
+        if($request->hasFile('imagen')){
+            $nombreArchivioOriginal= $request->file('imagen')->getClientOriginalName();
+            $nuevoNombre= Carbon::now()->timestamp."_".$nombreArchivioOriginal;
+            $carpetaDestino='./upload/';
+            $request->file('imagen')->move($carpetaDestino, $nuevoNombre);
+            $datosLibro->titulo= $request->titulo;
+            $datosLibro->imagen= ltrim($carpetaDestino,'.').$nuevoNombre;
+            $datosLibro->save();
+        }        
+        return response()->json($nuevoNombre);
+    }
+     
+    public function ver($id){
+
+        $datosLibro= new Libro;
+        $datosEncontrados=$datosLibro->find($id);
+
+        return response()->json($datosEncontrados);
+
+    }
+
+    public function eliminar($id){
+
+        $datosLibro= Libro::find($id);
+
+        if ($datosLibro) {
+            $rutaArchivo= base_path('public').$datosLibro->imagen;
+            if (file_exists($rutaArchivo)) {
+                unlink($rutaArchivo);
+            }
+            $datosLibro->delete();
+        }        
+
+        return response()->json("registro Borrado");
+
+    }
+
+    public function actualizar(Request $request, $id){
+
+        $datosLibro= Libro::find($id);
+
+        if($request->hasFile('imagen')){
+
+            if ($datosLibro) {
+                $rutaArchivo= base_path('public').$datosLibro->imagen;
+                if (file_exists($rutaArchivo)) {
+                    unlink($rutaArchivo);
+                }
+                $datosLibro->delete();
+            }
+
+            $nombreArchivioOriginal= $request->file('imagen')->getClientOriginalName();
+            $nuevoNombre= Carbon::now()->timestamp."_".$nombreArchivioOriginal;
+            $carpetaDestino='./upload/';
+            $request->file('imagen')->move($carpetaDestino, $nuevoNombre);
+            
+            $datosLibro->imagen= ltrim($carpetaDestino,'.').$nuevoNombre;
+            $datosLibro->save();
+        } 
+
+        if($request->input('titulo')){
+            $datosLibro->titulo=$request->input('titulo');
+        }
+
+        $datosLibro->save();
+
+        return response()->json($datosLibro);
+    }
+
+}
